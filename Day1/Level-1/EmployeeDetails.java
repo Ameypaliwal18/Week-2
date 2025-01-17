a1dbdc55a491673f133aaa460f57b490114b523d
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-17 13:03:25 +0530

    Employee Details

---
+++ Day1/Level-1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+// created the class Employee
+public class EmployeeDetails {
+    // Created the name id and Salary of the employee
+    String name;
+    int id;
+    double Salary;
+    // made function to display the details of the Employee
+    public  void  Display()
+    {
+        System.out.println(" The Name of the Employee is :"+ name);
+        System.out.println(" The Id of the EMmployee is :" + id);
+        System.out.println(" The Salary of the Employee is "+ Salary);
+    }
+
+
+    public static void main(String[] args) {
+        // declaration of oject of Employee Class and Instance
+        EmployeeDetails employee1= new EmployeeDetails();
+       // added  the name to the Employee Class using the .
+        employee1.name= "Amey";
+        employee1.id= 022;
+        employee1.Salary=100000;
+        // calling the Display fuction with help of object Display;
+        employee1.Display();
+
+
+
+    }
+}
