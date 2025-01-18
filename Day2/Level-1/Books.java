501118456f621dbd1c335ffedb1d0e3c2a149c66
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-18 15:41:26 +0530

    Book Class Question

---
+++ Day2/Level-1/Books.java
@@ -0,0 +1,47 @@
+public class Books {
+
+    public static void main(String[] args) {
+
+        System.out.println("Non parameterised constructor is called!!");
+        // Create a Book object using the default constructor
+        Book book = new Book();
+        // Display details of the book
+        book.displayBookDetails();
+
+        System.out.println();
+        System.out.println("Parameterised constructor is called!!");
+        // Create a Book object using the parameterized constructor
+        Book books = new Book("C++", "By me", 250.36);
+        // Display details of the book
+        books.displayBookDetails();
+    }
+}
+
+class Book {
+
+    // declare attributes of book
+    private String title;
+    private String author;
+    private double price;
+
+    // create a default constructor
+    public Book() {
+        title = "Java Fundamentals";
+        author = "Herbert Schildt";
+        price = 512.50;
+    }
+
+    // create a parameterized constructor to initialize values to it
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // call a method to display book details
+    public void displayBookDetails() {
+        System.out.println("The title of book is: " + title);
+        System.out.println("The author of book is: " + author);
+        System.out.println("The price of book is: " + price);
+    }
+}
