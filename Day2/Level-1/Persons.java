55ccdfac0a053ee17049d1b39adb545a60695ccc
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-20 10:27:55 +0530

    Persons name and  age

---
+++ Day2/Level-1/Persons.java
@@ -0,0 +1,39 @@
+public class Persons {
+
+    public static void main(String[] args) {
+        System.out.println("Parameterized constructor is called!!!");
+        // create object of person class with parameterized constructor
+        Person person = new Person("XYZ", 25);
+        // call a method to display details
+        person.displayDetails();
+        // create object of person class with copy constructor
+        Person persons = new Person(person);
+        // call a method to display details
+        persons.displayDetails();
+    }
+}
+
+class Person {
+
+    // declare variables
+    private String name;
+    private int age;
+
+    // create a parameterized constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // create a copy constructor
+    public Person(Person details) {
+        this.name = details.name;
+        this.age = details.age;
+    }
+
+    //method to display details
+    public void displayDetails() {
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
+    }
+}
