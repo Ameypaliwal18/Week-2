a84cef0102530eaff4ea1e161b102e5deeacec97
Ameypaliwal18 <ameypaliwal13@gmail.com>
2025-01-20 10:22:49 +0530

    Circle Radius

---
+++ Day2/Level-1/Circles.java
@@ -0,0 +1,35 @@
+public class Circles {
+
+    public static void main(String[] args) {
+        System.out.println("Default constructor is called!!!");
+        // create object of  Circle class with default constructor
+        Circle circle = new Circle();
+        // display details of circle class
+        circle.displayDetails();
+        System.out.println("Parameterized constructor is called!!!");
+        // create object of Circle class with parameterized constructor
+        Circle circles = new Circle(5.2);
+        // display details of circle class
+        circles.displayDetails();
+    }
+}
+
+class Circle {
+
+    private double radius;
+
+    // create a default constructor
+    public Circle() {
+        this(0.5); // constructor chaining
+    }
+
+    // create a parameterized constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // method to display details
+    public void displayDetails() {
+        System.out.println("Radius is: " + radius);
+    }
+}
